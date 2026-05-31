824ea674ac7d3ba4caf6d61a5586fa22fc525e39
Booklan <kmzhoushuyan@qq.com>
2026-05-31 18:50:39 +0800

    Auto deploy 2026-05-31 18:50:39

---
+++ src/components/client-providers.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const DynamicBackground = dynamic(() => import("@/components/dynamic-background"), {
+  ssr: false,
+  loading: () => <div className="fixed inset-0 z-0" />,
+});
+
+const RippleEffect = dynamic(() => import("@/components/RippleEffect"), {
+  ssr: false,
+});
+
+export default function ClientProviders({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <div className="fixed inset-0 overflow-hidden z-0">
+        <DynamicBackground />
+      </div>
+      {children}
+      <RippleEffect />
+    </>
+  );
+}
