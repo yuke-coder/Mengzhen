39b67d2154a4444a3fa88dc221e6c39f3ab5e1b5
Booklan <kmzhoushuyan@qq.com>
2026-05-31 18:34:25 +0800

    Auto deploy 2026-05-31 18:34:25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState, useCallback, useEffect, useRef } from "react";
-import dynamic from "next/dynamic";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
